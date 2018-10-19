9c8827947a7c0c7fe75c029ab99c0fd7bd210a2c
ZangZ <jinjin1_happy@163.com>
2018-10-19 16:59:45 +0800

    抽取app的创建逻辑到info模块

--- manage.py
+++ manage.py
@@ -1,30 +1,9 @@
-from flask import Flask,session
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CSRFProtect
-from redis import StrictRedis
-# 可以用来指定session的保存位置
-from flask_session import Session
+from flask import session
 from flask_script import Manager
 # 导入数据迁移
 from flask_migrate import Migrate, MigrateCommand
-from config import Config
+from info import app,db
 
-
-
-
-
-app = Flask(__name__)
-# 加载配置
-app.config.from_object(Config)
-
-# 初始化数据库
-db = SQLAlchemy(app)
-# 初始化 redis 存储对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# 开启当前项目 CSRF 保护，只做服务器验证功能
-CSRFProtect(app)
-# 设置session保存指定位置
-Session(app)
 
 manager = Manager(app)
 Migrate(app, db)
