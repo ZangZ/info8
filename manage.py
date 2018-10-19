1b1628a41f55e33cf42de20a955d5924dcb07fb3
ZangZ <jinjin1_happy@163.com>
2018-10-19 19:37:15 +0800

    导入模型类,数据迁移和测试数据导入

--- manage.py
+++ manage.py
@@ -1,8 +1,6 @@
-from flask import session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from info import create_app,db
-import logging
+from info import create_app,db,models
 
 
 # manager.py是程序启动的入口，只关心启动的相关参数以及内容，不关心具体该如何创建相关逻辑
@@ -12,9 +10,6 @@ manager = Manager(app)
 Migrate(app, db)
 # 将迁移命令添加到manager中
 manager.add_command("db", MigrateCommand)
-
-
-
 
 
 if __name__ == '__main__':
