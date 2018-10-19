ba572d519fc7eed5e2f7443bd88960353426b409
ZangZ <jinjin1_happy@163.com>
2018-10-19 17:37:03 +0800

    抽取app到函数中使配置传参

--- manage.py
+++ manage.py
@@ -2,9 +2,9 @@ from flask import session
 from flask_script import Manager
 # 导入数据迁移
 from flask_migrate import Migrate, MigrateCommand
-from info import app,db
+from info import create_app,db
 
-
+app = create_app("development")
 manager = Manager(app)
 Migrate(app, db)
 # 将迁移命令添加到manager中
@@ -14,7 +14,7 @@ manager.add_command("db", MigrateCommand)
 @app.route('/')
 def index():
     session["name"] = "itheima"
-    return 'index8888888'
+    return 'index888899'
 
 
 if __name__ == '__main__':
