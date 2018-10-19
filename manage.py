844140243289f6e5b9a6f07a7c807a1466b0e1a8
ZangZ <jinjin1_happy@163.com>
2018-10-19 16:23:59 +0800

    0.0.35修复了集成flask_script后的bug

--- manage.py
+++ manage.py
@@ -9,25 +9,34 @@ from flask_script import Manager
 
 
 
+# from flask.ext.wtf import CSRFProtect
+# from redis import StrictRedis
+# from flask import Flask, session
+# from flask.ext.sqlalchemy import SQLAlchemy
+# 可以用来指定 session 保存的位置
+# from flask_session import Session
+# from flask_script import Manager
 
 
-# 使用类对象创建配置类
-class Config():
+class Config(object):
+    """项目的配置"""
     DEBUG = True
 
-    SECRET_KEY = "tJrg2EE/sIdQVD8yZmrnCNEgOpYhItcbsNAlGO7MFyXJ+E70ewLsE+yTeLEjDl11ctxEHxXteRDM1GU2YM+AOw=="
-    # 为Mysql添加配置
+    SECRET_KEY = "iECgbYWReMNxkRprrzMo5KAQYnb2UeZ3bwvReTSt+VSESW0OB8zbglT+6rEcDW9X"
+
+    # 为数据库添加配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/info8"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # 为redis添加配置
+
+    # Redis的配置
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
-    # session保存配置
+    # Session保存配置
     SESSION_TYPE = "redis"
-    # 是否开启session签名
+    # 开启session签名
     SESSION_USE_SIGNER = True
-    # 指定Session保存的redis
+    # 指定 Session 保存的 redis
     SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
     # 设置需要过期
     SESSION_PERMANENT = False
@@ -35,18 +44,15 @@ class Config():
     PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
-
-
-
 app = Flask(__name__)
-
 # 加载配置
 app.config.from_object(Config)
+
 # 初始化数据库
 db = SQLAlchemy(app)
-# 初始化redis对象
-redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
-# 开启当前项目CSRF保护
+# 初始化 redis 存储对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 开启当前项目 CSRF 保护，只做服务器验证功能
 CSRFProtect(app)
 # 设置session保存指定位置
 Session(app)
@@ -54,13 +60,11 @@ Session(app)
 manager = Manager(app)
 
 
-
 @app.route('/')
 def index():
-    session["name"] = "INFO8"
-    return 'info8'
+    session["name"] = "itheima"
+    return 'index33333'
 
 
 if __name__ == '__main__':
     manager.run()
-
