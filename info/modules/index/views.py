2ffb6b0a5f3ed1f569ccbab6073a4913e3b91b92
ZangZ <jinjin1_happy@163.com>
2018-10-19 21:06:27 +0800

    加入网页标签栏小图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from info import redis_store
 from . import index_blu
@@ -8,3 +8,8 @@ from . import index_blu
 @index_blu.route("/")
 def index():
     return render_template("news/index.html")
+
+# 打开浏览器的时候浏览器会默认请求跟路由路径+ favicon.ico作为网站的小图标
+@index_blu.route("/favicon.ico")
+def favicon():
+    return current_app.send_static_file("news/favicon.ico")
