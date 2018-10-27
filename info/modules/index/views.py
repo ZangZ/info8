e87a03973c11d8ca979155d00bc92880f4ee0413
ZangZ <jinjin1_happy@163.com>
2018-10-27 21:15:01 +0800

    新闻详情页数据展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,16 @@
-from flask import render_template, current_app, session, request, jsonify
+from flask import current_app, jsonify
+from flask import g
+from flask import render_template
+from flask import request
+from flask import session
 
-from info import redis_store, constants
+from info import constants
+from info import redis_store
 from info.models import User, News, Category
+from info.utils.common import user_login_data
 from info.utils.response_code import RET
 from . import index_blu
+
 
 @index_blu.route('/news_list')
 def news_list():
@@ -40,13 +47,13 @@ def news_list():
         return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
 
     # 取到当前页的数据
-    news_list = paginate.items  # 模型对象列表
+    news_model_list = paginate.items  # 模型对象列表
     total_page = paginate.pages
     current_page = paginate.page
 
     # 将模型对象列表转成字典列表
     news_dict_li = []
-    for news in news_list:
+    for news in news_model_list:
         news_dict_li.append(news.to_basic_dict())
 
     data = {
@@ -56,7 +63,10 @@ def news_list():
     }
 
     return jsonify(errno=RET.OK, errmsg="OK", data=data)
+
+
 @index_blu.route('/')
+@user_login_data
 def index():
     """
     显示首页
@@ -64,19 +74,19 @@ def index():
     :return:
     """
     # 显示用户是否登录的逻辑
-
     # 取到用户id
-    user_id = session.get("user_id", None)
-    user = None
-    if user_id:
-        # 尝试查询用户的模型
-        try:
-            user = User.query.get(user_id)
-        except Exception as e:
-            current_app.logger.error(e)
+    # user_id = session.get("user_id", None)
+    # user = None
+    # if user_id:
+    #     # 尝试查询用户的模型
+    #     try:
+    #         user = User.query.get(user_id)
+    #     except Exception as e:
+    #         current_app.logger.error(e)
 
+    user = g.user
 
-    # 右侧新闻排行的逻辑
+    # 右侧的新闻排行的逻辑
     news_list = []
     try:
         news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
@@ -105,8 +115,8 @@ def index():
     return render_template('news/index.html', data=data)
 
 
-# 打开浏览器的时候浏览器会默认请求跟路由路径+ favicon.ico作为网站的小图标
-@index_blu.route("/favicon.ico")
+# 在打开网页的时候，浏览器会默认去请求根路径+favicon.ico作网站标签的小图标
+# send_static_file 是 flask 去查找指定的静态文件所调用的方法
+@index_blu.route('/favicon.ico')
 def favicon():
-    return current_app.send_static_file("news/favicon.ico")
-
+    return current_app.send_static_file('news/favicon.ico')
--- info/utils/common.py
+++ info/utils/common.py
@@ -1,10 +1,49 @@
+import functools
+# 共用的自定义工具类
+from flask import current_app
+from flask import g
+from flask import session
+from info.models import User
+
+
 def do_index_class(index):
-    """自定义过滤器，过滤点击排序html的class"""
+    """返回指定索引对应的类名"""
+
     if index == 0:
         return "first"
     elif index == 1:
         return "second"
     elif index == 2:
         return "third"
-    else:
-        return ""
+
+    return ""
+
+
+def user_login_data(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        user_id = session.get("user_id", None)
+        user = None
+        if user_id:
+            # 尝试查询用户的模型
+            try:
+                user = User.query.get(user_id)
+            except Exception as e:
+                current_app.logger.error(e)
+        # 把查询出来的数据赋值给g变量
+        g.user = user
+        return f(*args, **kwargs)
+    return wrapper
+
+
+# def query_user_data():
+#     user_id = session.get("user_id", None)
+#     user = None
+#     if user_id:
+#         # 尝试查询用户的模型
+#         try:
+#             user = User.query.get(user_id)
+#         except Exception as e:
+#             current_app.logger.error(e)
+#         return user
+#     return None
