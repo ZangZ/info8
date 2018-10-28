35d02d88854a9a8acbe37cc390a1dda9b5900e30
ZangZ <jinjin1_happy@163.com>
2018-10-28 16:23:58 +0800

    修改密码的前段实现

--- info/utils/image_storage.py
+++ info/utils/image_storage.py
@@ -4,6 +4,8 @@ access_key = "yV4GmNBLOgQK-1Sn3o4jktGLFdFSrlywR2C-hvsW"
 secret_key = "bixMURPL6tHjrb8QKVg2tm7n9k8C7vaOeQ4MEoeW"
 bucket_name = "ihome"
 
+# TODO access_key 和 bucket_name 不能用
+
 
 def storage(data):
     try:
@@ -12,7 +14,7 @@ def storage(data):
         ret, info = put_data(token, None, data)
         print(ret, info)
     except Exception as e:
-        raise e;
+        raise e
 
     if info.status_code != 200:
         raise Exception("上传图片失败")
