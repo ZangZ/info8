f21d9615eae978608c9d8d181905615fc654574f
ZangZ <jinjin1_happy@163.com>
2018-10-30 10:16:54 +0800

    如果管理员已登录,直接跳转到管理员主页

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -16,6 +16,11 @@ def index():
 @admin_blu.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "GET":
+        # 判断当前是否有登录,如果有登陆直接重定向到管理员后台主页
+        user_id = session.get('user_id', None)
+        is_admin = session.get("is_admin", False)
+        if user_id and is_admin:
+            return redirect(url_for('admin.index'))
         return render_template('admin/login.html')
 
     # 取到登录的参数
