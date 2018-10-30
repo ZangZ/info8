b578f8138d7353db2f94f32952fc43aca109712a
ZangZ <jinjin1_happy@163.com>
2018-10-30 09:48:06 +0800

    集成创建管理员的命令到命令行

--- manage.py
+++ manage.py
@@ -4,12 +4,36 @@ from info import create_app,db,models
 
 
 # manager.py是程序启动的入口，只关心启动的相关参数以及内容，不关心具体该如何创建相关逻辑
+from info.models import User
 
 app = create_app("development")
 manager = Manager(app)
 Migrate(app, db)
 # 将迁移命令添加到manager中
 manager.add_command("db", MigrateCommand)
+
+@manager.option('-n', '-name', dest="name")
+@manager.option('-p', '-password', dest="password")
+def createsuperuser(name, password):
+
+    if not all([name, password]):
+        print("参数不足")
+
+    user = User()
+    user.nick_name = name
+    user.mobile = name
+    user.password = password
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+
+    print("添加成功")
+
 
 
 if __name__ == '__main__':
